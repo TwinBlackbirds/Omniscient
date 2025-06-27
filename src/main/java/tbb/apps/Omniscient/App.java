9cb6e0a3c8f136a73669cd666534cd7ff163e273
Michael Amyotte <twinblackbirds@gmx.com>
2025-06-27 16:32:47 -0400

    Initial collector and dispatcher created

--- src/main/java/tbb/apps/Omniscient/App.java
+++ src/main/java/tbb/apps/Omniscient/App.java
@@ -8,8 +8,17 @@ package tbb.apps.Omniscient;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Spliterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -37,8 +46,21 @@ public class App
 	private static final int MAX_RETRIES = 3; // if page fails to load (cd.get())
 	private static final int TIMEOUT_SEC = 30; // time to wait for el to be present
 	private static final int EXTRA_WAIT_MS = 1000; // extra time spent waiting after el is present
+	private static final int MAX_CHILDREN = 10; // amount of child procs dispatch() is allowed to spawn
+	private static final int ARTICLES_PER_CHILD = 500;
 	
-	private static final String[] BANNED_URL_FRAGMENTS = {"/wiki/Help:", "/wiki/Special:"};
+	// url handling
+	private static final String[] BANNED_URL_FRAGMENTS = {
+			"/wiki/Help:", 
+			"/wiki/Special:",
+			"/wiki/Wikipedia:",
+			"/wiki/Category:",
+			"/wiki/File:",
+			"/wiki/Template:",
+			"/wiki/Template_talk:"
+	};
+	private static List<String> allLinks = new ArrayList<String>();
+	private static ConcurrentLinkedQueue<String> currentLinks = new ConcurrentLinkedQueue<String>();
 	
 	// db
 	private static Sqlite sql = new Sqlite(log, true);
@@ -75,18 +97,22 @@ public class App
     	cd = new ChromeDriver(cds, co);
     	js = (JavascriptExecutor) cd;
     	
+    	
     	// end-user feedback
     	Printer.startBox("Omniscient");
+    	// TODO: add how long the program has been running for in box TUI
+    	// TODO: also add how many urls have been grabbed and how many articles have been extracted
     	
     	// String s = loopUntilInput();
     	try {
     		// only enable while loop once you are confident in the bot's abilities
-//    		while (true) {
-            	bot();
+    		while (true) {
+            	bot(ARTICLES_PER_CHILD * MAX_CHILDREN); 
+            	dispatch();
             	try {
             		Thread.sleep(5000);
             	} catch (Exception e) {};
-//    		}
+    		}
     	} catch (Exception e) {
     		log.Write(LogLevel.ERROR, "Bot failed! " + e);
     	} finally {
@@ -101,24 +127,111 @@ public class App
     }
     
     // our 'spider'
-    private static void bot() throws Exception {
-    	navigateTo("https://en.wikipedia.org"); // we only want english articles
+    private static void bot(int amtOfLinks) throws Exception {
+    	log.Write(LogLevel.INFO, String.format("Starting spider process to collect %d links", amtOfLinks));
+    	int startingIdx;
+    	String navTo;
     	
+    	if (allLinks.size() < 1) {
+    		startingIdx = 0;
+    		navTo = "https://en.wikipedia.org"; // 'starting article' ability later on
+    	} else {
+    		startingIdx = allLinks.size()-1;
+    		navTo = allLinks.get(startingIdx);
+    	}
+    	int count = 0;
     	
+    	while (allLinks.size() < startingIdx + amtOfLinks) {
+    		navigateTo(navTo);
+        	List<String> links = getUniqueValidLinks();
+        	allLinks.addAll(links);
+        	currentLinks.addAll(links);
+        	navTo = allLinks.get(startingIdx + count);
+        	count++;
+    	}
     }
     
     // clean urls, split them up, and dispatch them to children processes
     private static void dispatch() throws Exception {
+    	// --- overview:
+    	// split URL list into chunks
+    	// spawn child processes to collect data from the URLs
+    	// block thread until all processes return
     	
+    	log.Write(LogLevel.INFO, "Dispatching to collector processes");
+    	ExecutorService es = Executors.newFixedThreadPool(MAX_CHILDREN);
+    	// give each child MAX_ARTICLES / 10 
+    	
+    	sendState(State.COLLECTING);
+    	List<Future<?>> tasks = new ArrayList<>();
+
+        // May have to lock access to currentLinks
+        while (!currentLinks.isEmpty()) {
+            String[] urls = grabRange(ARTICLES_PER_CHILD);
+            if (urls.length > 0) {
+                tasks.add(es.submit(() -> {
+                    try {
+                        scraper(urls);
+                    } catch (Exception e) {
+                        log.Write(LogLevel.ERROR, "Could not dispatch! " + e);
+                    }
+                }));
+            }
+        }
+
+        es.shutdown();
+
+        // Wait for all tasks to complete
+        for (Future<?> task : tasks) {
+            try {
+                task.get(); // this blocks until that specific task finishes
+            } catch (ExecutionException | InterruptedException e) {
+                log.Write(LogLevel.ERROR, "Task failed: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
+        
+    	currentLinks.clear(); // clear l1 cache, l2 cache is unaffected (allLinks)
+    	sendState(State.WAITING);
     }
     
     // this is the child process which scrapes a wiki 
     // TODO: figure out a plan of action for how to control the chrome driver without too much resource hogging
     // Share one chrome driver between many scrapers and just open new tabs perhaps?
-    private static void scraper() throws Exception {
-    	
+    private static void scraper(String[] urls) throws Exception {
+    	log.Write(LogLevel.INFO, String.format("Scraper received %d urls. There are %d left in currentLinks.", urls.length, currentLinks.size()));
     }
     
+    public static String[] grabRange(int amount) {
+    	
+    	int lastIdx = currentLinks.size() - 1;
+    	if (amount >= lastIdx) { // we are at the end
+    		return currentLinks.toArray(new String[0]);
+    	}
+    	
+        ArrayList<String> newList = new ArrayList<String>();
+        String pop = currentLinks.poll();
+        int count = 0;
+    	while (pop != null && count < amount)  {
+        	newList.add(pop);
+    		pop = currentLinks.poll();
+        }   
+    	return newList.toArray(new String[0]);
+    }
+    
+    private static List<String> getUniqueValidLinks() {
+    	sendState(State.COLLECTING);
+    	List<WebElement> links = cd.findElements(By.cssSelector("div.mw-content-ltr a[href*='/wiki']"));
+    	ArrayList<String> hrefs = new ArrayList<String>();
+    	for (WebElement link : links) {
+    		String href = link.getAttribute("href");
+    		if (isUrlEnglish(href) && isUrlWiki(href) && !allLinks.contains(href)) {
+    			hrefs.add(href);
+    		}
+    	}
+    	sendState(State.WAITING);
+    	return hrefs;
+    }
     
     private static void navigateTo(String URL) {
     	sendState(State.NAVIGATING);
@@ -140,17 +253,10 @@ public class App
     	sendState(State.WAITING);
     }
     
-    private static List<String> getOnlyEnglishPages(List<String> urls) {
-    	List<String> list = new ArrayList<>();
-    	for (String url : urls) {
-    		if (isUrlEnglish(url)) list.add(url);
-		}
-    	return list;
-    }
-    
     private static boolean isUrlWiki(String url) {
     	for (String frag : BANNED_URL_FRAGMENTS) {
     		if (url.contains(frag)) return false;
+    		if (!url.contains("wikipedia.org")) return false;
     	}
     	return true;
     }
@@ -170,6 +276,10 @@ public class App
     	}
     	cleanURL = cleanURL.split("/")[0];
     	Printer.sh.update(state, cleanURL);
+    }
+    
+    private static void sendRawState(State state) {
+    	Printer.sh.update(state, "data;,");
     }
     
     private static void jsClick(WebElement el) {

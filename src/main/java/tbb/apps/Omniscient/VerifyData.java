b0fb48736c1c8d8128a37e0b9f6a7289db93bef6
Michael Amyotte <twinblackbirds@gmx.com>
2025-07-10 11:58:47 -0400

    new class: experimental sub-program class VerifyData

---
+++ src/main/java/tbb/apps/Omniscient/VerifyData.java
@@ -0,0 +1,14 @@
+package tbb.apps.Omniscient;
+
+import tbb.db.Driver.Sqlite;
+import tbb.utils.Logger.Logger;
+
+public class VerifyData {
+	private static final Logger log = new Logger();
+	private static final Sqlite sql = new Sqlite(log);
+	
+	public static void verify() {
+		// walk through the database and report bad data (in both instances and wiki)
+		// perform summary of data as well
+	}
+}

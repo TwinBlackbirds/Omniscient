07df6788bd538944b4eb5a5e6e7dfcaf6fb2426f
Michael Amyotte <twinblackbirds@gmx.com>
2025-07-16 10:32:59 -0400

    basic database verification solution

--- src/main/java/tbb/apps/Omniscient/VerifyData.java
+++ src/main/java/tbb/apps/Omniscient/VerifyData.java
@@ -1,15 +1,33 @@
 package tbb.apps.Omniscient;
 
 import tbb.db.Driver.Sqlite;
+import tbb.utils.Logger.LogLevel;
 import tbb.utils.Logger.Logger;
 
 public class VerifyData {
-	private static final Logger log = new Logger();
-	private static final Sqlite sql = new Sqlite(log);
+	private final static Logger log = new Logger(VerifyData.class, LogLevel.INFO);
+	private static final Sqlite sql = new Sqlite(log, false);
 	
-	public static void verify() {
+	public static boolean verify() {
 		// walk through the database and report bad data (in both instances and wiki)
 		// perform summary of data as well
-		sql.countWikis();
+		// start first by loading all into memory, optimize later
+		DatabaseCriteria c = new DatabaseCriteria(log);
+		
+		// first check: database exists
+		try {
+			c.setExists(sql.checkDbExists());
+		} catch (Exception e) {
+			log.Write(LogLevel.ERROR, "Database file does not exist! Database could not be verified.");
+			return false;
+		}
+		
+		// second check: database has entries
+		int cnt = sql.countWikis();
+		c.setCount(cnt);
+		log.Write(LogLevel.INFO, "Amount of wikis recognized: " + cnt);
+		
+		
+		return c.checkCriteria();
 	}
 }

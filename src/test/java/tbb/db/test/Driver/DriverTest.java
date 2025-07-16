d68de08284480c2fc20a4a399375dbecfe3bc67d
Michael Amyotte <twinblackbirds@gmx.com>
2025-07-16 10:32:47 -0400

    add database verification test suite

--- src/test/java/tbb/db/test/Driver/DriverTest.java
+++ src/test/java/tbb/db/test/Driver/DriverTest.java
@@ -3,6 +3,7 @@ package tbb.db.test.Driver;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import tbb.apps.Omniscient.VerifyData;
 
 /**
  * Unit test for simple App.
@@ -30,7 +31,7 @@ public class DriverTest extends TestCase
     
     public void testApp()
     {
-    	// TODO: add tests
-    	assertTrue(true);
+    	boolean dbVerified = VerifyData.verify();
+    	assertTrue(dbVerified);
     }
 }

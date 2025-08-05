6cf943cd95232cde13bea4436294b7e8cb5adc04
Michael Amyotte <twinblackbirds@gmx.com>
2025-08-05 09:48:08 -0400

    Fix silly build error <>

--- src/main/java/tbb/utils/Logger/FileDeleter.java
+++ src/main/java/tbb/utils/Logger/FileDeleter.java
@@ -8,7 +8,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileDeleter {
-	private static final SimpleFileVisitor<Path> sfv = new SimpleFileVisitor<>() {
+	private static final SimpleFileVisitor<Path> sfv = new SimpleFileVisitor<Path>() {
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
             throws IOException
